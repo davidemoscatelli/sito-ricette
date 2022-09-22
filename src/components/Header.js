98d753fbbdadc859a2d55cbe2135031f4314c0b5
“davidemoscatelli” <davidemoscatelli1995@gmail.com>
2022-09-22 21:20:45 +0200

    hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,18 +20,7 @@ const PageHeader = () => (
                                 </Link>
                             </div>
                             <div className="-ml-2 mr-2 flex items-center md:hidden">
-                               <Disclosure.Button
-                                    className="inline-flex items-center justify-center p-2 rounded-md text-gray-400
-                                        hover:text-gray-500 hover:bg-gray-100 focus:outline-none
-                                        focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-                                >
-                                    <span className="sr-only">Open main menu</span>
-                                    {open ? (
-                                        <XIcon className="block h-6 w-6" aria-hidden="true" />
-                                    ) : (
-                                        <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                                    )}
-                                </Disclosure.Button>
+                              
                                 
                             </div>
                         </div>
