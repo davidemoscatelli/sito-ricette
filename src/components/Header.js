991c53c2daba7bb15e0bea00a73243a7fea18f50
“davidemoscatelli” <davidemoscatelli1995@gmail.com>
2022-09-22 21:18:10 +0200

    modifica immagine mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,7 @@ const PageHeader = () => (
                                 </Link>
                             </div>
                             <div className="-ml-2 mr-2 flex items-center md:hidden">
-                              /*  <Disclosure.Button
+                               <Disclosure.Button
                                     className="inline-flex items-center justify-center p-2 rounded-md text-gray-400
                                         hover:text-gray-500 hover:bg-gray-100 focus:outline-none
                                         focus:ring-2 focus:ring-inset focus:ring-indigo-500"
@@ -32,7 +32,7 @@ const PageHeader = () => (
                                         <MenuIcon className="block h-6 w-6" aria-hidden="true" />
                                     )}
                                 </Disclosure.Button>
-                                */
+                                
                             </div>
                         </div>
                         <div className="order-3 md:order-2 hidden md:flex items-center justify-center w-full">
