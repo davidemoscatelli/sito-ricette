38d5d613148247356a76758d747d17c75722623b
“davidemoscatelli” <davidemoscatelli1995@gmail.com>
2022-09-22 22:14:31 +0200

    ultime modifiche

---
+++ .cache/async-requires.js
@@ -0,0 +1,16 @@
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=default" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js?export=default" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-templates-index-js": () => import("./../../../src/templates/index.js?export=default" /* webpackChunkName: "component---src-templates-index-js" */),
+  "component---src-templates-recipe-js": () => import("./../../../src/templates/recipe.js?export=default" /* webpackChunkName: "component---src-templates-recipe-js" */)
+}
+
+
+
+exports.head = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=head" /* webpackChunkName: "component---cache-dev-404-page-jshead" */),
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js?export=head" /* webpackChunkName: "component---src-pages-404-jshead" */),
+  "component---src-templates-index-js": () => import("./../../../src/templates/index.js?export=head" /* webpackChunkName: "component---src-templates-index-jshead" */),
+  "component---src-templates-recipe-js": () => import("./../../../src/templates/recipe.js?export=head" /* webpackChunkName: "component---src-templates-recipe-jshead" */)
+}
+
