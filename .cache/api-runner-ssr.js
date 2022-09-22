d492c129b268bbbce11379a7e45e1dc3c281d34a
“davidemoscatelli” <davidemoscatelli1995@gmail.com>
2022-09-22 20:34:29 +0200

    PUBLIC

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,30 +1,30 @@
 var plugins = [{
       name: 'gatsby-plugin-google-gtag',
-      plugin: require('/Users/davidemoscatelli/test-sito/node_modules/gatsby-plugin-google-gtag/gatsby-ssr.js'),
+      plugin: require('/Users/davidemoscatelli/Progetti/sito-ricette/node_modules/gatsby-plugin-google-gtag/gatsby-ssr.js'),
       options: {"plugins":[],"trackingIds":["test"],"gtagConfig":{"optimize_id":"OPT_CONTAINER_ID","anonymize_ip":true,"cookie_expires":0},"pluginConfig":{"head":true,"respectDNT":true,"exclude":["/preview/**","/do-not-track/me/too/"]}},
     },{
       name: 'gatsby-plugin-react-helmet',
-      plugin: require('/Users/davidemoscatelli/test-sito/node_modules/gatsby-plugin-react-helmet/gatsby-ssr.js'),
+      plugin: require('/Users/davidemoscatelli/Progetti/sito-ricette/node_modules/gatsby-plugin-react-helmet/gatsby-ssr.js'),
       options: {"plugins":[]},
     },{
       name: 'gatsby-plugin-sitemap',
-      plugin: require('/Users/davidemoscatelli/test-sito/node_modules/gatsby-plugin-sitemap/gatsby-ssr.js'),
+      plugin: require('/Users/davidemoscatelli/Progetti/sito-ricette/node_modules/gatsby-plugin-sitemap/gatsby-ssr.js'),
       options: {"plugins":[],"output":"/sitemap","createLinkInHead":true,"entryLimit":45000,"query":"{ site { siteMetadata { siteUrl } } allSitePage { nodes { path } } }","excludes":[]},
     },{
       name: 'gatsby-plugin-image',
-      plugin: require('/Users/davidemoscatelli/test-sito/node_modules/gatsby-plugin-image/gatsby-ssr.js'),
+      plugin: require('/Users/davidemoscatelli/Progetti/sito-ricette/node_modules/gatsby-plugin-image/gatsby-ssr.js'),
       options: {"plugins":[]},
     },{
       name: 'gatsby-plugin-manifest',
-      plugin: require('/Users/davidemoscatelli/test-sito/node_modules/gatsby-plugin-manifest/gatsby-ssr.js'),
+      plugin: require('/Users/davidemoscatelli/Progetti/sito-ricette/node_modules/gatsby-plugin-manifest/gatsby-ssr.js'),
       options: {"plugins":[],"icon":"src/assets/favicons/favicon-16x16.png","icons":[{"src":"src/assets/favicons/favicon-32x32.png","sizes":"32x32","type":"image/png"},{"src":"src/assets/favicons/favicon-16x16.png","sizes":"16x16","type":"image/png"}],"legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"f6e7ad074d96fc505981a5636044eb44"},
     },{
       name: 'gatsby-plugin-gatsby-cloud',
-      plugin: require('/Users/davidemoscatelli/test-sito/node_modules/gatsby-plugin-gatsby-cloud/gatsby-ssr.js'),
+      plugin: require('/Users/davidemoscatelli/Progetti/sito-ricette/node_modules/gatsby-plugin-gatsby-cloud/gatsby-ssr.js'),
       options: {"plugins":[],"headers":{"/*":["x-frame-options: allow-from https://jamstackthemes.dev/"]}},
     },{
       name: 'partytown',
-      plugin: require('/Users/davidemoscatelli/test-sito/node_modules/gatsby/dist/internal-plugins/partytown/gatsby-ssr.js'),
+      plugin: require('/Users/davidemoscatelli/Progetti/sito-ricette/node_modules/gatsby/dist/internal-plugins/partytown/gatsby-ssr.js'),
       options: {"plugins":[]},
     }]
 /* global plugins */
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,9 +4,9 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/davidemoscatelli/test-sito/.cache/dev-404-page.js")),
-  "component---src-pages-404-js": preferDefault(require("/Users/davidemoscatelli/test-sito/src/pages/404.js")),
-  "component---src-templates-index-js": preferDefault(require("/Users/davidemoscatelli/test-sito/src/templates/index.js")),
-  "component---src-templates-recipe-js": preferDefault(require("/Users/davidemoscatelli/test-sito/src/templates/recipe.js"))
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/davidemoscatelli/Progetti/sito-ricette/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/davidemoscatelli/Progetti/sito-ricette/src/pages/404.js")),
+  "component---src-templates-index-js": preferDefault(require("/Users/davidemoscatelli/Progetti/sito-ricette/src/templates/index.js")),
+  "component---src-templates-recipe-js": preferDefault(require("/Users/davidemoscatelli/Progetti/sito-ricette/src/templates/recipe.js"))
 }
 
