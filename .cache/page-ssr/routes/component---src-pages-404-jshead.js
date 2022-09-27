9a8dabcf06eabd7e1ce43aab74ca5dae4f246cb0
davide moscatelli <davidemoscatelli1995@gmail.com>
2022-09-27 20:24:30 +0200

    file netlify config api

--- .cache/page-ssr/routes/component---src-pages-404-jshead.js
+++ .cache/page-ssr/routes/component---src-pages-404-jshead.js
@@ -3273,7 +3273,7 @@ const navigation = {
       viewBox: "0 0 13 25"
     }, props), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("path", {
       fillRule: "evenodd",
-      d: "M8.30466 24.1211V13.5346H11.8761L12.407 9.38964H8.30466V6.74947C8.30466 5.55339 8.63791 4.73447 10.3545 4.73447H12.5297V1.03902C11.4714 0.925596 10.4076 0.870832 9.34316 0.874974C6.18633 0.874974 4.01891 2.80214 4.01891 6.34002V9.38189H0.470703V13.5268H4.02666V24.1211H8.30466Z",
+      d: "M8.30466 24.1211V13.5346H11.8761L12.407 9.38964H8.30466V6.74947C8.30466\r 5.55339 8.63791 4.73447 10.3545 4.73447H12.5297V1.03902C11.4714 0.925596\r 10.4076 0.870832 9.34316 0.874974C6.18633 0.874974 4.01891 2.80214 4.01891\r 6.34002V9.38189H0.470703V13.5268H4.02666V24.1211H8.30466Z",
       clipRule: "evenodd"
     }))
   }, {
@@ -3294,7 +3294,7 @@ const navigation = {
       fill: "currentColor",
       viewBox: "0 0 24 24"
     }, props), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("path", {
-      d: "M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
+      d: "M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022\r 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0\r 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8\r 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233\r 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
     }))
   }, {
     name: 'Link',
@@ -3304,11 +3304,11 @@ const navigation = {
       viewBox: "0 0 26.68 11.63"
     }, props), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("path", {
       fillRule: "evenodd",
-      d: "M15.06,0a5.79,5.79,0,0,0-4.75,2.46A5.62,5.62,0,0,0,9.3,5a5.8,5.8,0,0,0,.27,2.74H11.7a3.89, 3.89,0,0,1-.52-1.94,3.83,3.83,0,0,1,.52-1.93,3.88,3.88,0,0,1,3.36-1.94h5.81a3.88,3.88,0,0,1,0, 7.75h-3a8,8,0,0,1-1.58,1.94h4.56a5.83,5.83,0,0,0,2.22-.45A5.75,5.75,0,0,0,25,9.92,5.81,5.81,0,0, 0,26.24,8a5.85,5.85,0,0,0,0-4.45A5.79,5.79,0,0,0,23.09.44,5.82,5.82,0,0,0,20.87,0Z",
+      d: "M15.06,0a5.79,5.79,0,0,0-4.75,2.46A5.62,5.62,0,0,0,9.3,5a5.8,5.8,0,0,0,.27,2.74H11.7a3.89,\r 3.89,0,0,1-.52-1.94,3.83,3.83,0,0,1,.52-1.93,3.88,3.88,0,0,1,3.36-1.94h5.81a3.88,3.88,0,0,1,0,\r 7.75h-3a8,8,0,0,1-1.58,1.94h4.56a5.83,5.83,0,0,0,2.22-.45A5.75,5.75,0,0,0,25,9.92,5.81,5.81,0,0,\r 0,26.24,8a5.85,5.85,0,0,0,0-4.45A5.79,5.79,0,0,0,23.09.44,5.82,5.82,0,0,0,20.87,0Z",
       clipRule: "evenodd"
     }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("path", {
       fillRule: "evenodd",
-      d: "M10.37,0H5.81a5.82,5.82,0,0,0,0,11.63h5.81A5.92,5.92,0,0,0,14.3,11a5.87,5.87,0,0,0,3.09-4.37, 5.88,5.88,0,0,0-.28-2.73H15.5a1.51,1.51,0,0,0-.48.06,3.91,3.91,0,0,1,.48,1.92A3.81,3.81,0,0,1, 15,7.78a3.85,3.85,0,0,1-3.34,1.91H5.81a3.88,3.88,0,1,1,0-7.75h3A7.64,7.64,0,0,1,10.38,0Z",
+      d: "M10.37,0H5.81a5.82,5.82,0,0,0,0,11.63h5.81A5.92,5.92,0,0,0,14.3,11a5.87,5.87,0,0,0,3.09-4.37,\r 5.88,5.88,0,0,0-.28-2.73H15.5a1.51,1.51,0,0,0-.48.06,3.91,3.91,0,0,1,.48,1.92A3.81,3.81,0,0,1,\r 15,7.78a3.85,3.85,0,0,1-3.34,1.91H5.81a3.88,3.88,0,1,1,0-7.75h3A7.64,7.64,0,0,1,10.38,0Z",
       clipRule: "evenodd"
     }))
   }]
@@ -3317,7 +3317,7 @@ const navigation = {
 const Footer = () => /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("footer", {
   className: "flex flex-col items-center justify-center"
 }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
-  className: "md:w-auto px-5 md:px-0 mt-8 flex flex-col items-center space-y-5 md:space-y-8"
+  className: "md:w-auto px-5 md:px-0 mt-8\r flex flex-col items-center space-y-5 md:space-y-8"
 }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(gatsby__WEBPACK_IMPORTED_MODULE_1__.Link, {
   to: "/"
 }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(flotiq_components_react__WEBPACK_IMPORTED_MODULE_2__.Image, {
@@ -3338,7 +3338,7 @@ const Footer = () => /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().c
   className: "h-6 w-6",
   "aria-hidden": "true"
 }))))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
-  className: "font-sora w-auto px-2 md:px-5 py-5 mt-20 inline-flex justify-between md:justify-center items-center bg-light-gray rounded-t-lg"
+  className: "font-sora w-auto px-2 md:px-5 py-5 mt-20\r inline-flex justify-between md:justify-center items-center\r bg-light-gray rounded-t-lg"
 }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("p", {
   className: "text-center text-xs md:text-base font-light"
 }, "Copyright \xA9 DM 2022")));
@@ -3401,7 +3401,7 @@ const PageHeader = () => /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default
   className: "flex-shrink-0"
 }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("a", {
   href: "mailto:hello@domain.com",
-  className: "relative inline-flex items-center px-4 py-2 text-base md:text-lg font-regular hover:text-secondary"
+  className: "relative inline-flex items-center px-4 py-2\r text-base md:text-lg font-regular hover:text-secondary"
 }, "Contattami!"))))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_headlessui_react__WEBPACK_IMPORTED_MODULE_4__.Disclosure.Panel, {
   className: "md:hidden"
 }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
